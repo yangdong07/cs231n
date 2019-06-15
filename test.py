971d51523b0d63bde014a51ae4dbb0e7a0a0c496
yangdong <yangdong07@gmail.com>
2019-06-15 15:07:54 +0800

    assignment1, 2

---
+++ test.py
@@ -0,0 +1,25 @@
+
+
+from torch.utils.data import Dataset, DataLoader, sampler
+
+from torchvision import transforms, datasets
+
+
+import torchvision
+
+
+from torch.utils.data import SubsetRandomSampler
+
+
+# torchvision.utils.make_grid
+
+torchvision.datasets.CIFAR10()
+
+
+import torch.nn as nn
+
+
+
+import torchvision.models as models
+
+alexnet = models.alexnet()
